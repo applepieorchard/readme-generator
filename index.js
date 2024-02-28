493459ca1010f8dd5e0d987c021d85ff57e09479
applepieorchard <paul.miclaus1229@gmail.com>
2024-02-28 19:25:19 +0000

    Added user input questions to the questions array

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  "What is the title of your project?",
+  "Write a description for your project",
+  "Describe any installation instructions",
+  "How do you use your application?",
+  "What license is your project subject to?",
+  "What contribution guidelines do you wish to add?",
+  "Any testing instructions for your application?",
+  "What is your github username?",
+  "What is your email address?"
 ];
 
 // function to write README file
 const writeToFile = (fileName, data) => {
-    
+  
 }
 
 // function to initialize program
 const init = () => {
-
+  
 }
 
 // function call to initialize program
