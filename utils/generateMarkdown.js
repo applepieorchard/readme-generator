75638c89a6e041884343a0061c880f687394fc07
applepieorchard <paul.miclaus1229@gmail.com>
2024-02-29 18:26:47 +0000

    created the generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,45 @@
 // function to generate markdown for README
 const generateMarkdown = (data) => {
-  return `# ${data.title}
+  return `
+    # ${data.title}
+    
+    ## Description
+    
+    ${data.description}
 
-`;
+    ## Table of Contents
+
+    [Installation](#installation)
+    [Usage](#usage)
+    [License](#license)
+    [Contributing](#contributing)
+    [Tests](#tests)
+    [Questions](#questions)
+
+    ## Installation
+
+    ${data.installation}
+
+    ## Usage
+
+    ${data.usage}
+
+    ## License
+
+    This project is subject to the [${data.license}](LICENSE).
+
+    ## Contributing
+
+    ${data.contributing}
+
+    ## Tests
+
+    ${data.tests}
+
+    ## Questions
+
+    If you have any questions you can contact me on github [@${data.github}](https://github.com/${data.github}) or email at [${data.email}](mailto:${data.email})
+  `;
 }
 
 module.exports = generateMarkdown;
